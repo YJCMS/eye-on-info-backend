97d990a30421dcb76f84c68724b84bab7e2722e4
YJCMS <sen9563@gmail.com>
2025-05-06 11:46:02 +0900

    delete: unused feature

--- utils/image_utils.py
+++
@@ -1,9 +0,0 @@
-import os
-from config import Config
-
-def ensure_directory_exists():
-    if not os.path.exists(Config.SAVE_DIR):
-        os.makedirs(Config.SAVE_DIR)
-        
-def get_headers():
-    return {'User-Agent': Config.USER_AGENT}
